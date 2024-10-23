ec3f0ca5838866daab3e1554d5e1cf3f7df3549c
Edmund Dong <12579441+EdmundDong@users.noreply.github.com>
2024-10-23 02:18:55 -0700

    Initial layout mockup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+  import icon from '$lib/img/icon.svg';
+</script>
+
+<nav class="sticky-top">
+  <header class="py-2 bg-body-tertiary border-bottom">
+    <div class="container d-flex flex-wrap justify-content-center">
+      <a
+        href="/"
+        class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none"
+      >
+        <img
+          src={icon}
+          class="me-2"
+          width="40"
+          height="32"
+          alt="Team CAUTION 1492 Robotics Team Logo"
+        />
+        <h1 class="fs-4 mb-0">Team CAUTION 1492</h1>
+      </a>
+      <ul class="nav me-auto"></ul>
+      <ul class="nav">
+        <li class="nav-item">
+          <a
+            href="#"
+            class="nav-link link-body-emphasis px-2 active"
+            aria-current="page">Home</a
+          >
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link link-body-emphasis px-2">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link link-body-emphasis px-2">Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link link-body-emphasis px-2">Off-Season</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link link-body-emphasis px-2">Support</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link link-body-emphasis px-2">Get Involved</a>
+        </li>
+      </ul>
+    </div>
+  </header>
+</nav>
+
+<slot />
+
+<footer class="py-2 bg-body-tertiary">
+  <div class="d-flex justify-content-center">
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <a href="#" class="nav-link" aria-label="Facebook Icon"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-youtube"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"
+            />
+          </svg></a
+        >
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link" aria-label="Facebook Icon"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-facebook"
+            viewBox="0 0 16 16"
+            aria-label="Facebook Icon"
+          >
+            <path
+              d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"
+            />
+          </svg></a
+        >
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-twitter-x"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"
+            />
+          </svg></a
+        >
+      </li>
+    </ul>
+  </div>
+  <div class="d-flex justify-content-center">
+    Made by Team CAUTION with love ♥
+  </div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+  import icon from '$lib/img/icon.svg';
+</script>
+
+<div class="container">
+  <div class="my-3">
+    <h5>
+      <span>Welcome to Team CAUTION!</span>
+    </h5>
+  </div>
+  <div class="my-3">
+    <p>
+      <span
+        >In the final weeks of the 2004 season registration period, an Arizona
+        high school decided they were no longer going to support their robotics
+        team.&nbsp;However, two students and two&nbsp;mentors from the team
+        wanted to continue their participation in the program. Microchip
+        graciously step in and offered them a workspace in one of their
+        warehouses to build their robot, and thus the Arizona Community Robotics
+        Team, also known as Team CAUTION, was born. It was founded as a
+        community team giving students whose schools do not have FIRST teams an
+        opportunity to participate in the program.</span
+      >
+    </p>
+  </div>
+  <div class="my-3">
+    <img src={icon} alt="Team CAUTION 1492 Robotics Team Logo" />
+  </div>
+</div>
